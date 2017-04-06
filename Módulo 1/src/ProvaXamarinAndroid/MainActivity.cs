22e2d7dcfbcecb14b6beefbe689cf20c48c5bef4
Anderson Dias <andersondiasfsa@gmail.com>
2017-04-06 00:39:10 -0300

    LAB 01 - Concluido!

---
+++ Módulo 1/src/ProvaXamarinAndroid/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ProvaXamarinAndroid
+{
+    [Activity(Label = "ProvaXamarinAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int contador = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            contador += 1;
+
+            Button bntOla = base.FindViewById<Button>(Resource.Id.bntOla);
+
+            bntOla.Click += delegate { bntOla.Text = string.Concat("Contador de clicks: ", contador); };
+        }
+
+    }
+}
+
